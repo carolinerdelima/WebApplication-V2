c51b973de5da3addebd95df5a30fb8bfdf8332a2
Caroline Lima <caroline.lima01@edu.pucrs.br>
2022-10-05 21:35:17 -0300

    Inserindo novas entidades

--- Persistência/Entidade/Enrollment.cs
+++ Persistência/Entidade/Enrollment.cs
@@ -1,4 +1,7 @@
-﻿namespace Persistencia.Entidade
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Persistencia.Entidade
 {
     public enum Grade
     {
@@ -8,11 +11,16 @@
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
+
         public int CourseID { get; set; }
+
         public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
+
         public Student Student { get; set; }
     }
 }
